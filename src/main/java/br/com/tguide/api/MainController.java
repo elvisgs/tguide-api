b484700002c66e94791b92236b888446caf35fb6
Elvis Luciano <elvis.gs@gmail.com>
2017-03-20 13:09:46 -0300

    Correção do base path do endpoint ratings

--- src/main/java/br/com/tguide/api/MainController.java
+++ src/main/java/br/com/tguide/api/MainController.java
@@ -10,15 +10,13 @@ import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController("/ratings")
+@RestController
+@RequestMapping("/ratings")
 public class MainController {
 
     private static final String FUSEKI_URI = "http://eudesdionatas.tk:8080/TGuide";
